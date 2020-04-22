17502fb84057b50feb424bf19bc048a37883413e
treasurechic <babychic140@gmail.com>
2020-04-22 22:25:20 +0100

    make it better

--- server.js
+++ server.js
@@ -5,14 +5,17 @@
 const express = require('express')
 const app  = express()
 const expresLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expresLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit:'10mb', extended:false }))
 
 const mongoose = require('mongoose') 
 mongoose.connect('mongodb://localhost/mybrary', {useNewUrlParser:true})
@@ -21,5 +24,6 @@ db.on('error', error=> console.error(error))
 db.once('open', () => console.log('connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
